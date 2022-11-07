8cddaa4f32818bbc1258fcd8039473f7b65d0e96
ПАВЕЛ <P.Kalitin@mail.ru>
2022-11-07 15:41:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task7/Frag1.kt
@@ -0,0 +1,29 @@
+package com.example.task7
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class Frag1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_frag1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button).setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,SekondFragment())
+        }
+    }
+
+
+    }
+
+
